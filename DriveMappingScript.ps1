88838de27ef06c515b7313fb6de5644f2dbc9a89
soeminnwin <53594460+soeminnwin@users.noreply.github.com>
2019-08-02 13:34:44 +0800

    Create DriveMappingScript.ps1

---
+++ DriveMappingScript.ps1
@@ -0,0 +1,80 @@
+Start-Transcript -Path $(Join-Path $env:temp "DriveMapping.log")
+
+$driveMappingConfig=@()
+
+######################################################################
+#                section script configuration                        #
+######################################################################
+
+<#
+   Add your internal Active Directory Domain name and custom network drives below
+#>
+
+$dnsDomainName= "pacificlight.local"
+
+$driveMappingConfig+= [PSCUSTOMOBJECT]@{
+    DriveLetter = "I"
+    UNCPath= "http://doclight/IT/IT%20Library"
+    Description="IT Library"
+}
+
+$driveMappingConfig+=  [PSCUSTOMOBJECT]@{
+    DriveLetter = "R"
+    UNCPath= "\\SNG-FS-01\PacificLight Singapore"
+    Description="PacificLight Singapore"
+}
+
+$driveMappingConfig+=  [PSCUSTOMOBJECT]@{
+    DriveLetter = "S"
+    UNCPath= "\\SNG-FS-01\PacificLight Power (City)"
+    Description="PacificLight Power (City)"
+}
+
+$driveMappingConfig+=  [PSCUSTOMOBJECT]@{
+    DriveLetter = "T"
+    UNCPath= "\\SNG-FS-01\Historic Files from IG"
+    Description="Historic Files from IG"
+}
+
+######################################################################
+#               end section script configuration                     #
+######################################################################
+
+$connected=$false
+$retries=0
+$maxRetries=3
+
+Write-Output "Starting script..."
+do {
+    
+    if (Resolve-DnsName $dnsDomainName -ErrorAction SilentlyContinue){
+    
+        $connected=$true
+
+    } else{
+ 
+        $retries++
+        
+        Write-Warning "Cannot resolve: $dnsDomainName, assuming no connection to fileserver"
+ 
+        Start-Sleep -Seconds 3
+ 
+        if ($retries -eq $maxRetries){
+            
+            Throw "Exceeded maximum numbers of retries ($maxRetries) to resolve dns name ($dnsDomainName)"
+        }
+    }
+ 
+}while( -not ($Connected))
+
+#Map drives
+    $driveMappingConfig.GetEnumerator() | ForEach-Object {
+
+        Write-Output "Mapping network drive $($PSItem.UNCPath)"
+
+        New-PSDrive -PSProvider FileSystem -Name $PSItem.DriveLetter -Root $PSItem.UNCPath -Description $PSItem.Description -Persist -Scope global
+
+        (New-Object -ComObject Shell.Application).NameSpace("$($PSItem.DriveLetter):").Self.Name=$PSItem.Description
+}
+
+Stop-Transcript
